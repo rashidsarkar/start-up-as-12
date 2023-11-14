1bbce9e999b97aaf38c5d6dcee8974cf38549a77
rashidsarkar <rashidrock558@gmail.com>
2023-11-14 19:51:06 +0600

    Update API base URL to http://localhost:5000 (generated by useblackbox.ai )

--- src/AxiosAPI/axiosInstance.js
+++ src/AxiosAPI/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://educoda-server.vercel.app", // Your API base URL
+  baseURL: "http://localhost:5000", // Your API base URL
   // baseURL: "http://localhost:5000", // Your API base URL
   withCredentials: true,
 });
--- src/AxiosAPI/useAxiosInstance.js
+++ src/AxiosAPI/useAxiosInstance.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"; // Import useNavigate from the a
 import useAuthProvider from "../FireBase/useAuthProvider";
 
 const axiosInstance = axios.create({
-  baseURL: "https://educoda-server.vercel.app", // Your API base URL
+  baseURL: "http://localhost:5000", // Your API base URL
   // baseURL: "http://localhost:5000", //> Your API base URL
   withCredentials: true,
 });
